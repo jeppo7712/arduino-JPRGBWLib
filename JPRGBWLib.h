03fd8c51dfe13734cc10164a9545dbf52e6b218b
jeppo77 <jeppo77@yahoo.com>
2019-01-19 18:50:49 +0100

    Update JPRGBWLib.h

--- JPRGBWLib.h
+++ JPRGBWLib.h
@@ -43,6 +43,7 @@ public:
         boolean getfading();
 
         void hsb2rgbw(uint16_t hue, uint8_t sat, uint8_t bri, uint8_t *r, uint8_t *g, uint8_t *b, uint8_t *w);
+        void hsb2rgbw_v2(uint16_t hue, uint8_t sat, uint8_t bri, uint8_t *r, uint8_t *g, uint8_t *b, uint8_t *w);
         void rgbw2hsb(uint8_t r, uint8_t g, uint8_t b, uint8_t w, uint16_t *hue, uint8_t *sat, uint8_t *bri);
 
 private:
